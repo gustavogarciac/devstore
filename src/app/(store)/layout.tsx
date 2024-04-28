6e796b964cdfe4848ed3ff6d2ee73f49f0887beb
gustavo <gustavogarciaci1@gmail.com>
2024-04-28 20:21:27 -0300

    feat: cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,11 +1,14 @@
 import { Header } from '@/components/header'
+import { CartContextProvider } from '@/contexts/cart-context'
 import { PropsWithChildren } from 'react'
 
 export default function StoreLayout(props: PropsWithChildren) {
   return (
-    <div className="mx-auto grid grid-rows-[min-content_max-content] gap-5 p-8 min-h-screen w-full max-w-[1600px]">
-      <Header />
-      {props.children}
-    </div>
+    <CartContextProvider>
+      <div className="mx-auto grid grid-rows-[min-content_max-content] gap-5 p-8 min-h-screen w-full max-w-[1600px]">
+        <Header />
+        {props.children}
+      </div>
+    </CartContextProvider>
   )
 }
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import React from 'react'
 
@@ -19,6 +20,24 @@ interface ProductProps {
   params: {
     slug: string
   }
+}
+
+export async function generateMetadata({
+  params,
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug)
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
 }
 
 const ProductPage = async ({ params }: ProductProps) => {
