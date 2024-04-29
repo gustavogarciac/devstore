d3d608a57b472b512810d14e0ed23f15e44303d6
gustavo <gustavogarciaci1@gmail.com>
2024-04-28 22:21:53 -0300

    feat: input search

---
+++ src/components/search-form.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { FormEvent } from 'react'
+
+export const SearchForm = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  function handleSearch(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const data = Object.fromEntries(formData)
+
+    const query = data.q
+    if (!query) return null
+
+    router.push(`/search?q=${query}`)
+  }
+
+  const query = searchParams.get('q')
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+    >
+      <Search className="w-5 h-5 text-zinc-500" />
+
+      <input
+        name="q"
+        defaultValue={query ?? ''}
+        placeholder="Buscar produtos..."
+        className="flex-1 bg-transparent outline-none text-sm placeholder:text-zinc-500"
+      />
+    </form>
+  )
+}
